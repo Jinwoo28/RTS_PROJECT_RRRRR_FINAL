94a7e875aef4f306a3bd31f1be68d39504d4d1b0
Jinwoo28 <91857768+Jinwoo28@users.noreply.github.com>
2021-12-13 17:50:13 +0900

    진우

---
+++ Assets/Scripts/S_JJW/ShowFeedbackPointer.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace Graphics.Feedback.Scripts
+
+{
+    public class ShowFeedbackPointer : MonoBehaviour
+    {
+        private readonly FeedbackPointer _feedbackPointer = new FeedbackPointer();
+
+        [SerializeField]
+        private float _feedbackPointerScale = 1f;
+
+        [SerializeField]
+        private GameObject _moveToIndicator;
+
+
+        public void InteractOnPerformed(Vector2 position)
+        {
+           
+
+         
+
+  
+        }
+
+        private void Start()
+        {
+            _feedbackPointer.PreparePointer(_moveToIndicator, _feedbackPointerScale);
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+                InteractOnPerformed(Input.mousePosition);
+        }
+    }
+}
